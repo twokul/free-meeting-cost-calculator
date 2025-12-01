70f352dbf31c8a8f948d5d20b3a319f3e171817c
Alex Navasardyan <alex.navasardyan@gmail.com>
2025-11-30 21:39:01 -0800

    add padding

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="py-6 text-center text-sm text-neutral-500">
+    <footer className="py-6 text-center text-sm text-neutral-500 px-2">
       Meeting data is processed locally. No data is stored. Free Meeting Cost
       Calculator by{" "}
       <a
